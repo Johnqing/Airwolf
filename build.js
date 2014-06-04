09c09ae4a778c5013fdbb9469305ba5bf04e6147
johnqing <csssnow@gmail.com>
2014-06-04 18:56:38 +0800

    airwolf

---
+++ build.js
@@ -0,0 +1,115 @@
+var fs = require('fs')
+	, path = require('path');
+
+/**
+ * 文件写入
+ * @param file
+ * @param content
+ */
+function writeFile(file, content, encode, fn){
+	if(typeof encode == 'function'){
+		fn = encode;
+		encode = void 0;
+	}
+
+	fs.open(file, "w", 0666, function(e, fd){
+		if(e) throw e;
+		fs.write(fd, content, 0, (encode || 'utf-8'), function(e){
+			if(e) throw e;
+			fn && fn();
+			fs.closeSync(fd);
+		});
+	});
+}
+
+// lib dir
+
+var lib = 'lib/components';
+var dest = 'dest/ui.js';
+
+// js
+writeFile(dest, '', 'utf-8');
+var js = fs.createWriteStream('dest/ui.js', {
+	flags: 'a'
+});
+
+// components to build
+
+var components = ['emitter', 'dialog', 'overlay'];
+
+function next(i) {
+	var name = components[i];
+	if (!name) return;
+	build(name, function(){
+		next(++i);
+	});
+}
+
+// build em!
+
+console.log('START!\n');
+
+var basejs = fs.readFileSync('lib/base.js', 'utf-8');
+var classjs = fs.readFileSync('lib/class.js', 'utf-8');
+
+js.write(basejs + '\n');
+js.write(classjs + '\n');
+next(0);
+process.on('exit', function(){
+	console.log();
+});
+
+/**
+ * Build the given component.
+ */
+
+function build(name, fn) {
+	// javascript
+	var js = path.join(lib, name, 'index.js');
+	read(js, function(js){
+
+		// with template
+		var html = path.join(lib, name, 'index.html');
+		if (fs.existsSync(html)) {
+			read(html, function(html){
+				js = '\n;(function(af, html){\n'
+					+ js
+					+ '\n})(af, ' + JSON.stringify(html) + ');';
+				append(dest, js, function(){
+					console.log('  \033[90mbuild \033[36m%s\033[m', name);
+					fn();
+				});
+			});
+			// without template
+		} else {
+			js = '\n;(function(af){\n'
+				+ js
+				+ '\n})(af);';
+			append(dest, js, function(){
+				console.log('  \033[90mbuild \033[36m%s\033[m', name);
+				fn();
+			});
+		}
+	});
+}
+
+/**
+ * Append to `file`.
+ */
+
+function append(file, str, fn) {
+	fs.createWriteStream(file, { flags: 'a' })
+		.write(str);
+	fn && fn();
+}
+
+/**
+ * Read the given `file`.
+ */
+
+function read(file, fn) {
+	fs.readFile(file, 'utf8', function(err, str){
+		if (err) throw err;
+		fn(str);
+	});
+}
